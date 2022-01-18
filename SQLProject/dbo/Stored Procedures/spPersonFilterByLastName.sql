06e455b4586bc0afa875c555876d1a8137f52e59
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2022-01-18 23:50:26 +0100

    Add project files.

---
+++ SQLProject/dbo/Stored Procedures/spPersonFilterByLastName.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spPersonFilterByLastName]
+	@LastName nvarchar(50)
+AS
+	begin
+	select [PersonId], [FirstName], [LastName] 
+	from dbo.Person
+	where LastName=@LastName;
+	end
